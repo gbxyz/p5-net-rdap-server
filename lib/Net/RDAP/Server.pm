ffc80c25b178308787a09b80287d91089cc2882c
Gavin Brown <gavin.brown@fastmail.uk>
2024-10-23 09:39:42 +0100

    bump version

--- lib/Net/RDAP/Server.pm
+++ lib/Net/RDAP/Server.pm
@@ -13,7 +13,7 @@ use strict;
 use vars qw($VERSION @METHODS @OBJECTS @SEARCHES @TYPES);
 use warnings;
 
-$VERSION    = '0.01';
+$VERSION    = '0.02';
 @METHODS    = qw(HEAD GET);
 @OBJECTS    = qw(domain nameserver entity ip autnum);
 @SEARCHES   = qw(domains nameservers entities);
